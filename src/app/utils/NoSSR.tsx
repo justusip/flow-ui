9f98c653a03140fab34daf3c67d1c133fa6c0935
Justus Ip <justusip@yahoo.com.hk>
2023-10-09 00:53:27 +0800

    Initial Commit

--- src/app/utils/NoSSR.tsx
+++ src/app/utils/NoSSR.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import React, {useEffect, useState} from "react";
 
-const NoSSR = (props: React.PropsWithChildren) => {
+const NoSSR = (props: React.PropsWithChildren): React.ReactNode => {
     const [isMounted, setIsMounted] = useState(false);
     useEffect(() => setIsMounted(true), []);
 
