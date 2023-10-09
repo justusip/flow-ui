3249941edfdd7725e341a31b924a2d7c215584ef
Justus Ip <justusip@yahoo.com.hk>
2023-10-09 13:36:43 +0800

    Initial Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,7 @@ export default function RootLayout({children}: {
     return (
         <html lang="en">
         <body className={inter.className}>
-        <NoSSR>
-            {children}
-        </NoSSR>
+        {children}
         </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@
 import dynamic from "next/dynamic";
 
 export default function Page() {
-    // const App = dynamic(
-    //     () => import("@/app/app"),
-    //     {ssr: false}
-    // );
-    // return <App/>;
-    return <div className={"text-white p-4"}>hi</div>;
+    const App = dynamic(
+        () => import("@/app/app"),
+        {ssr: false}
+    );
+    return <App/>;
+    // return <div className={"text-white p-4"}>hi</div>;
 };
