dd4567c66c0528861935245030a7b8093936f009
Justus Ip <justusip@yahoo.com.hk>
2023-10-09 11:04:55 +0800

    Initial Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,6 @@ export default function RootLayout({children}: {
         <html lang="en">
         <body className={inter.className}>
         <NoSSR>
-            {children}
         </NoSSR>
         </body>
         </html>
--- src/app/utils/NoSSR.tsx
+++ src/app/utils/NoSSR.tsx
@@ -1,22 +1,18 @@
 "use client";
 
 import React, {useEffect, useState} from "react";
-import dynamic from "next/dynamic";
 
 const NoSSR = (props: React.PropsWithChildren): React.ReactNode => {
-    // const [isMounted, setIsMounted] = useState(false);
-    //
-    // useEffect(() => {
-    //     setIsMounted(true);
-    // }, []);
-    //
-    // if (!isMounted)
-    //     return <></>;
-    //
-    // return <>{props.children}</>;
-    return props.children;
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted)
+        return <></>;
+
+    return <>{props.children}</>;
 };
 
-export default dynamic(() => Promise.resolve(NoSSR), {
-    ssr: false,
-});
+export default NoSSR;
