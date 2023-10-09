6b7769df688cab564196722d28acf9f41b0e0abf
Justus Ip <justusip@yahoo.com.hk>
2023-10-09 13:20:19 +0800

    Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@
 import dynamic from "next/dynamic";
 
 export default function Page() {
-    const App = dynamic(
-        () => import("@/app/app"),
-        {ssr: false}
-    );
-    return <App/>;
+    // const App = dynamic(
+    //     () => import("@/app/app"),
+    //     {ssr: false}
+    // );
+    // return <App/>;
+    return <div className={"text-white p-4"}>hi</div>;
 };
