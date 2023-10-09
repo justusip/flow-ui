653695dbba139741e6f6965330f33bee1056bb74
Justus Ip <justusip@yahoo.com.hk>
2023-10-09 11:02:50 +0800

    Initial Commit

--- src/app/utils/NoSSR.tsx
+++ src/app/utils/NoSSR.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import React, {useEffect, useState} from "react";
+import dynamic from "next/dynamic";
 
 const NoSSR = (props: React.PropsWithChildren): React.ReactNode => {
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
-
-    if (!isMounted)
-        return <></>;
-
-    return <>{props.children}</>;
+    // const [isMounted, setIsMounted] = useState(false);
+    //
+    // useEffect(() => {
+    //     setIsMounted(true);
+    // }, []);
+    //
+    // if (!isMounted)
+    //     return <></>;
+    //
+    // return <>{props.children}</>;
+    return props.children;
 };
 
-export default NoSSR;
+export default dynamic(() => Promise.resolve(NoSSR), {
+    ssr: false,
+});
